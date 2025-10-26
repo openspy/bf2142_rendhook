5ede94cab95b3f23da36b4d8899be090fe28a0e9
CHC NiZ <chc@openspy.net>
2025-10-25 23:13:29 -0400

    misc cleanup

--- FESL_impl.cpp
+++ FESL_impl.cpp
@@ -160,37 +160,34 @@ void SSL_Flush(FESLCtx* ctx) {
 		BIO_read(g_write_bio, &ctx->ssl_state->send_buffer, ssl_write_sz);
 
 		int r = send(ctx->fesl_socket->socket, (const char*)&ctx->ssl_state->send_buffer, ssl_write_sz, 0);
-		fprintf(console_fd, "write send: %d\n", r);
-	}
-	int ssl_read_sz = BIO_pending(g_read_bio);
-	if (ssl_read_sz > 0) {
-		BIO_read(g_read_bio, &ctx->ssl_state->send_buffer, ssl_write_sz);
-
-		int r = send(ctx->fesl_socket->socket, (const char*)&ctx->ssl_state->send_buffer, ssl_write_sz, 0);
-		fprintf(console_fd, "read send: %d\n", r);
+		if (r < 0) {
+			ctx->connection_state = 4099;
+			ctx->got_error = 1;
+			return;
+		}
 	}
 }
 void SSL_Read(FESLCtx* ctx) {
 	int r = recv(ctx->fesl_socket->socket, (char*)&ctx->ssl_state->recv_buffer, sizeof(ctx->ssl_state->recv_buffer), 0);
+	
 	if (r < 0) {
-		fprintf(console_fd, "recv error: %d\n", r);
-		//ctx->connection_state = 4097;
-		//ctx->got_error = 1;
+		int wserr = WSAGetLastError();
+		if (wserr != WSAEWOULDBLOCK) {
+			ctx->connection_state = 4097;
+			ctx->got_error = 1;
+		}
 	}
 	else if (r > 0) {
 		BIO_write(g_read_bio, (char*)&ctx->ssl_state->recv_buffer, r);
 		if (!SSL_is_init_finished(g_ssl)) {
 			SSL_Flush(ctx);
 		}
-		else {
-			fprintf(console_fd, "INIT FINISHED, DO SSL READ\n");
-		}
 	}
 }
 
 int SSL_LogicThread(FESLCtx* ctx) {
-	//fprintf(console_fd, "SSL_LogicThread: %d - %d\n", ctx->connection_state, ctx->fesl_socket->socket);
-	//fflush(console_fd);
+	fprintf(console_fd, "SSL_LogicThread: %d - %d\n", ctx->connection_state, ctx->fesl_socket->socket);
+	fflush(console_fd);
 	if (ctx->connection_state == 2) {
 		//unsigned long mode = 1; // 1 for non-blocking, 0 for blocking
 		//if (ioctlsocket(ctx->fesl_socket->socket, FIONBIO, &mode) != 0) {
@@ -265,12 +262,10 @@ int SSL_LogicThread(FESLCtx* ctx) {
 		int recvbuf[4096];
 		while (true) {
 			int r = recv(ctx->fesl_socket->socket, (char*)&recvbuf[0], sizeof(recvbuf), 0);
-			fprintf(console_fd, "RECV: %d\n", r);
 			if (r <= 0) {
 				break;
 			}
 			BIO_write(g_read_bio, (char*)&recvbuf[0], r);
-			fprintf(console_fd, "recv bio write: %d\n", r);
 		}
 		int read_len = sizeof(ctx->ssl_state->recv_buffer) - ctx->ssl_state->recv_buffer_cursor;
 		while (true) {
@@ -278,13 +273,12 @@ int SSL_LogicThread(FESLCtx* ctx) {
 			if (sr <= 0) {
 				break;
 			}
-			fprintf(console_fd, "expect: %d\n", sr);
 			ctx->ssl_state->recv_expected_len += sr;
 			show_dump((unsigned char*)&ctx->ssl_state->recv_buffer[ctx->ssl_state->recv_buffer_cursor], sr, console_fd);
 		}
 		fflush(console_fd);
-
 	}
+	SSL_Flush(ctx);
 	return ctx->connection_state;
 }
 
@@ -302,8 +296,6 @@ int fesl_SSL_Send(FESLCtx* ctx, char* buf, int len) {
 }
 
 int fesl_SSL_recv(FESLCtx* ctx, char* buf, int len) {
-	fprintf(console_fd, "fesl_SSL_recv - %d\n", len);
-	
 	int result = 0;
 	if (ctx->connection_state == 30) {
 		SSL_LogicThread(ctx);
@@ -320,13 +312,11 @@ int fesl_SSL_recv(FESLCtx* ctx, char* buf, int len) {
 			ctx->ssl_state->recv_buffer_cursor = 0;
 			ctx->ssl_state->recv_expected_len = 0;
 			ctx->ssl_state->recv_current_len = 0;
-			fprintf(console_fd, "did reset: %d\n", read_len);
 		}
 		result = read_len;
 		show_dump((unsigned char *)buf, read_len, console_fd);
 		
 	}
-	fprintf(console_fd, "RECV RESULT - %d\n", result);
 	
 	fflush(console_fd);
 	if (result > 0 && result < len)
@@ -334,9 +324,9 @@ int fesl_SSL_recv(FESLCtx* ctx, char* buf, int len) {
 	return result;
 }
 
-int verify_callback(int preverify_ok, X509_STORE_CTX* ctx) {
-	return 1;
-}
+//int verify_callback(int preverify_ok, X509_STORE_CTX* ctx) {
+//	return 1;
+//}
 void install_fesl_patches() {
 	SSL_library_init();
 	OpenSSL_add_all_algorithms();
@@ -345,7 +335,7 @@ void install_fesl_patches() {
 
 
 	g_ssl_ctx = SSL_CTX_new(TLS_method());
-	SSL_CTX_set_verify(g_ssl_ctx, SSL_VERIFY_PEER, verify_callback);
+	//SSL_CTX_set_verify(g_ssl_ctx, SSL_VERIFY_PEER, NULL); //call this to enable verification
 	//SSL_CTX_set_min_proto_version(g_ssl_ctx, TLS1_3_VERSION);
 	//SSL_CTX_set_max_proto_version(g_ssl_ctx, TLS1_3_VERSION);
 
